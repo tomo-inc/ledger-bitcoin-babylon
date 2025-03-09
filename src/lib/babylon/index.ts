b48a66f32c8dc5a97898a782d26d71dec167034d
steven <steven@tomo.inc>
2025-03-09 10:26:50 +0800

    update policy names, and add example code for step5 - staking transaction

--- src/lib/babylon/index.ts
+++ src/lib/babylon/index.ts
@@ -406,7 +406,7 @@ export async function timelockPathPolicy({
   return new WalletPolicy(policyName, descriptorTemplate, keys);
 }
 
-export type StakingTxPolicy = undefined | 'Staking Tx';
+export type StakingTxPolicy = undefined | 'Staking transaction';
 export type StakingTxParams = {
   timelockBlocks: number;
   finalityProviderPk: string;
@@ -415,7 +415,7 @@ export type StakingTxParams = {
 };
 
 export async function stakingTxPolicy({
-  policyName = 'Staking Tx',
+  policyName = 'Staking transaction',
   transport,
   params,
   derivationPath,
@@ -588,13 +588,14 @@ export async function tryParsePsbt(
 // // Method 1: Explicitly pass all required parameters to construct the policy.
 // async function testStakingStep1() {
 //   const psbt = base64.decode(
-//     'cHNidP8BAH0CAAAAAZUPGfxRcPueN3/UdNQC64mF3lAumoEi9Gv6AgvbdVycAAAAAAD/////AsQJAAAAAAAAFgAUW+EmJNCKK0JAldfAciHDNFDRS/EEpgAAAAAAACJRICyVutUKY9E6qBjfjktoZBga2/RyCoiq+OPBI1ugik2fAAAAAAABAStQwwAAAAAAACJRIEOj7UvRXfRV9er0SUNeReHNqaiqtOoEhmW60JCFUoUyQhXAUJKbdMGgSVS3i0tgNel6XgeKWg8o7JbVR7/ums6AOsCJtgX5iDHD5SbZ6yF5ZRRSk4qMD/f16u7MthJR1dRt6/15ASDcjS+e/wxPTb3gcKSOMw78kItip2ZWjZHmWPKEsyS4eK0gH5MjVzLmTKwzVprRw9vwQTgsO3dPz7BTO5sx1MKna/mtIAruBQmxbbccmZI4pIJ9uUVSaFmxPJVIerRnJTV8mp8lrCARPDoyqdMgtyGQoEoCCg2zl27zaXJnMljpo4o2Tz3DsLogF5Ic8VbMtOc9Qo+ZbtEbJFMT434nyXisTSzCHspGcuS6IDu5PfyLYYh9dx82MOmmPpfLr8/MeFVqR034OjGg74mcuiBAr69HxP+lbehkENjke6ortvBLYE9OokMjc33cP+CS37ogeacf/XHFA+8uL5G8z8j82nlG9GU87w2fPd4geV7zufC6INIfr3jGdRoNOOa9gCi5B/8H6ahppD/IN9az+N/2EZo2uiD1GZ764/KLuCR2Fjp+RYx61EXZv/sGgtENO9sstB+Ojrog+p2ILUX0BgvbgEIYOCjNh1RPHqmXOA5YbKt31f1phze6VpzAARcgUJKbdMGgSVS3i0tgNel6XgeKWg8o7JbVR7/ums6AOsAAAAA=',
+//     'cHNidP8BAH0CAAAAAZUPGfxRcPueN3/UdNQC64mF3lAumoEi9Gv6AgvbdVycAAAAAAD/////AsQJAAAAAAAAFgAUW+EmJNCKK0JAldfAciHDNFDRS/EEpgAAAAAAACJRICyVutUKY9E6qBjfjktoZBga2/RyCoiq+OPBI1ugik2fAAAAAAABAStQwwAAAAAAACJRIEOj7UvRXfRV9er0SUNeReHNqaiqtOoEhmW60JCFUoUyQhXAUJKbdMGgSVS3i0tgNel6XgeKWg8o7JbVR7/ums6AOsCJtgX5iDHD5SbZ6yF5ZRRSk4qMD/f16u7MthJR1dRt6/15ASDcjS+e/wxPTb3gcKSOMw78kItip2ZWjZHmWPKEsyS4eK0gH5MjVzLmTKwzVprRw9vwQTgsO3dPz7BTO5sx1MKna/mtIAruBQmxbbccmZI4pIJ9uUVSaFmxPJVIerRnJTV8mp8lrCARPDoyqdMgtyGQoEoCCg2zl27zaXJnMljpo4o2Tz3DsLogF5Ic8VbMtOc9Qo+ZbtEbJFMT434nyXisTSzCHspGcuS6IDu5PfyLYYh9dx82MOmmPpfLr8/MeFVqR034OjGg74mcuiBAr69HxP+lbehkENjke6ortvBLYE9OokMjc33cP+CS37ogeacf/XHFA+8uL5G8z8j82nlG9GU87w2fPd4geV7zufC6INIfr3jGdRoNOOa9gCi5B/8H6ahppD/IN9az+N/2EZo2uiD1GZ764/KLuCR2Fjp+RYx61EXZv/sGgtENO9sstB+Ojrog+p2ILUX0BgvbgEIYOCjNh1RPHqmXOA5YbKt31f1phze6VpzAARcgUJKbdMGgSVS3i0tgNel6XgeKWg8o7JbVR7/ums6AOsAAAAA='
 //   );
 
 //   const transport = await getLedgerTransport();
 
 //   const leafHash = computeLeafHash(psbt);
-//   const finalityProviderPk = '1f93235732e64cac33569ad1c3dbf041382c3b774fcfb0533b9b31d4c2a76bf9';
+//   const finalityProviderPk =
+//     '1f93235732e64cac33569ad1c3dbf041382c3b774fcfb0533b9b31d4c2a76bf9';
 //   const covenantPks = [
 //     '0aee0509b16db71c999238a4827db945526859b13c95487ab46725357c9a9f25',
 //     '113c3a32a9d320b72190a04a020a0db3976ef36972673258e9a38a364f3dc3b0',
@@ -615,7 +616,7 @@ export async function tryParsePsbt(
 //   };
 
 //   const policy = await slashingPathPolicy({
-//     policyName: 'Stake / Step 1',
+//     policyName: 'Step 1: slashing consent',
 //     transport,
 //     params,
 //     derivationPath: `m/86'/1'/0'`,
@@ -635,4 +636,41 @@ export async function tryParsePsbt(
 //   const policy = await tryParsePsbt(transport, psbtBase64, true);
 
 //   await signPsbt({ transport, psbt: psbtBase64, policy: policy! });
+// }
+
+// async function testStakingTx() {
+//   const psbt = base64.decode('');
+
+//   const transport = await getLedgerTransport();
+
+//   const finalityProviderPk =
+//     '1f93235732e64cac33569ad1c3dbf041382c3b774fcfb0533b9b31d4c2a76bf9';
+//   const covenantPks = [
+//     '0aee0509b16db71c999238a4827db945526859b13c95487ab46725357c9a9f25',
+//     '113c3a32a9d320b72190a04a020a0db3976ef36972673258e9a38a364f3dc3b0',
+//     '17921cf156ccb4e73d428f996ed11b245313e37e27c978ac4d2cc21eca4672e4',
+//     '3bb93dfc8b61887d771f3630e9a63e97cbafcfcc78556a474df83a31a0ef899c',
+//     '40afaf47c4ffa56de86410d8e47baa2bb6f04b604f4ea24323737ddc3fe092df',
+//     '79a71ffd71c503ef2e2f91bccfc8fcda7946f4653cef0d9f3dde20795ef3b9f0',
+//     'd21faf78c6751a0d38e6bd8028b907ff07e9a869a43fc837d6b3f8dff6119a36',
+//     'f5199efae3f28bb82476163a7e458c7ad445d9bffb0682d10d3bdb2cb41f8e8e',
+//     'fa9d882d45f4060bdb8042183828cd87544f1ea997380e586cab77d5fd698737',
+//   ];
+
+//   const params = {
+//     timelockBlocks: 64000,
+//     finalityProviderPk,
+//     covenantThreshold: 6,
+//     covenantPks,
+//   };
+
+//   const policy = await stakingTxPolicy({
+//     policyName: 'Staking transaction',
+//     transport,
+//     params,
+//     derivationPath: `m/86'/1'/0'`,
+//     isTestnet: true,
+//   });
+
+//   await signPsbt({ transport, psbt, policy });
 // }
