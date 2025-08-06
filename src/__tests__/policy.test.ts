1d22aae58de0ca5774a7f22edf844157f73dfddb
eshangui <eshangui@gmail.com>
2025-08-06 12:15:06 +0800

    update slashing test data

--- src/__tests__/policy.test.ts
+++ src/__tests__/policy.test.ts
@@ -48,7 +48,7 @@ describe('stakingTxPolicy', () => {
   it('should create slashing tx policy and send TLV data', async () => {
     const params = {
       timelockBlocks: 1008,
-      finalityProviders: ['1f93235732e64cac33569ad1c3dbf041382c3b774fcfb0533b9b31d4c2a76bf9'],
+      finalityProviders: ['d66124f8f42fd83e4c901a100ae3b5d706ef6cfd217b04bc64152e739a30c41e'],
       covenantThreshold: 6,
       covenantPks: [
         '0aee0509b16db71c999238a4827db945526859b13c95487ab46725357c9a9f25',
@@ -57,12 +57,12 @@ describe('stakingTxPolicy', () => {
         '3bb93dfc8b61887d771f3630e9a63e97cbafcfcc78556a474df83a31a0ef899c',
         '40afaf47c4ffa56de86410d8e47baa2bb6f04b604f4ea24323737ddc3fe092df',
         '79a71ffd71c503ef2e2f91bccfc8fcda7946f4653cef0d9f3dde20795ef3b9f0',
-        '8c1e7e2b5c6d4f1a2b3c4d5e6f7a8b9c0d1e2f3a4b5c6d7e8f9a0b1c2d3e4f5a',
-        '9f8e7d6c5b4a39281716151413121110ffeeddccbbaa99887766554433221100',
-        'abcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcd',
+        'd21faf78c6751a0d38e6bd8028b907ff07e9a869a43fc837d6b3f8dff6119a36',
+        'f5199efae3f28bb82476163a7e458c7ad445d9bffb0682d10d3bdb2cb41f8e8e',
+        'fa9d882d45f4060bdb8042183828cd87544f1ea997380e586cab77d5fd698737',
       ],
       slashingFeeSat: 2000,
-      leafHash: Buffer.from('00145be12624d08a2b424095d7c07221c33450d14bf1', 'hex'),
+      leafHash: Buffer.from('ed429f93af8bb724a9f5066248b32d945fdd1c12f7f59a33f4f83b6565716750', 'hex'),
       slashingPkScriptHex: "00145be12624d08a2b424095d7c07221c33450d14bf1",
     };
 
